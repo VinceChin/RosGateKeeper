ca4316cee5a8c569bb20114b7543285aebfb333e
vince <zhihaojin43@gmail.com>
2024-01-17 07:49:39 -0800

    Initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/vince/catkin_ws/src
+# Build directory: /home/vince/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("custom_cam_pkg")
+subdirs("speech_recognition")
---
+++ build/custom_cam_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vince/catkin_ws/src/custom_cam_pkg
+# Build directory: /home/vince/catkin_ws/build/custom_cam_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
